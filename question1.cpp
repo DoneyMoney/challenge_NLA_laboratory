fbf5e3f452851ce1e436e054eb8240f61878b6c3
giolagioia <giovanni.lagioia@mail.polimi.it>
2024-09-30 20:27:26 +0200

    Add files and solved question1.cpp

---
+++ question1.cpp
@@ -0,0 +1,23 @@
+#include <Eigen/Eigen>
+#include <iostream>
+#include <cstdlib>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+int main(){
+    const char* input_image_path = "256px-Albert_Einstein_Head.jpg";
+
+    int width, height, channels;
+    unsigned char* image_data = stbi_load(input_image_path, &width, &height, &channels, 1);
+    
+    if (!image_data) {
+    std::cerr << "Error: Could not load image " << input_image_path << std::endl;
+    return 1;
+    }
+
+    std::cout << "Image loaded: " << width << "x" << height << " with " << channels << " channels." << std::endl;
+    return 0;
+}
