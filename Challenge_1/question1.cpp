b919e901659cf855faef5b2b9a1a74d5759a723c
LucaLeonx <luca.leonzio@mail.polimi.it>
2024-10-01 09:07:18 +0000

    Folders re-org

--- Challenge_1/question1.cpp
+++ Challenge_1/question1.cpp
@@ -3,12 +3,12 @@
 #include <cstdlib>
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include "../external_libraries/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
+#include "../external_libraries/stb_image_write.h"
 
 int main(){
-    const char* input_image_path = "256px-Albert_Einstein_Head.jpg";
+    const char* input_image_path = "assets/256px-Albert_Einstein_Head.jpg";
 
     int width, height, channels;
     unsigned char* image_data = stbi_load(input_image_path, &width, &height, &channels, 1);
