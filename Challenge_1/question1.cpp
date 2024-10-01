40193a21f94a22c7207f71a7233cb33950990576
giolagioia <giovanni.lagioia@mail.polimi.it>
2024-10-01 19:59:25 +0200

    Solved issue with output path

--- Challenge_1/question1.cpp
+++ Challenge_1/question1.cpp
@@ -7,6 +7,8 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "../external_libraries/stb_image_write.h"
 
+using namespace Eigen;
+
 int main(){
     const char* input_image_path = "assets/256px-Albert_Einstein_Head.jpg";
 
@@ -18,28 +20,37 @@ int main(){
     return 1;
     }
 
-    Eigen::MatrixXd originalEinstenMat(height,width);
+    Eigen::MatrixXd originalEinsteinMat(height,width);
     for(int i=0;i<height;i++){
         for(int j=0;j<width;j++){
             int index = (i * width + j) * channels; 
-            originalEinstenMat(i,j) = static_cast<double>(image_data[index]);
+            originalEinsteinMat(i,j) = static_cast<double>(image_data[index]) / 255.0;
         }
     }
 
     //Point 1
     std::cout << "Image loaded: " << width << "x" << height << " with " << channels << " channels." << std::endl;
     //std::cout << "Matrix of the original image: " << originalEinsteinMat << std::endl;
-    std::cout << "Size of the matrix based on the image: " <<originalEinstenMat.size() << std::endl;
+    std::cout << "Size of the matrix based on the image: " <<originalEinsteinMat.size() << std::endl;
     
+    Matrix<unsigned char, Dynamic, Dynamic, RowMajor> output_image(width, height);
+  // Use Eigen's unaryExpr to map the grayscale values (0.0 to 1.0) to 0 to 255
+    output_image = originalEinsteinMat.unaryExpr([](double val) -> unsigned char {
+    return static_cast<unsigned char>(val * 255.0);
+    });
+
     //Check if the first images is translated correctly
-    const std::string output_image_path = "outputImages/testFirstOutput.png";
-    if (stbi_write_png(output_image_path.c_str(), width, height, 1, originalEinstenMat.data(), width) == 0) {
+    const std::string output_image_path = "testFirstOutput.png";
+
+    if (stbi_write_png(output_image_path.c_str(), width, height, 1, output_image.data(), width) == 0) {
     std::cerr << "Error: Could not save grayscale image" << std::endl;
 
     return 1;
   }
 
 
+
+    std::cout << "Images saved to " << output_image_path << " and " << output_image_path << std::endl;
     stbi_image_free(image_data);
 
     return 0;
