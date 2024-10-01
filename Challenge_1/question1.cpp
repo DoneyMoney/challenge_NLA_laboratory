d36260d3c33053310daf1511b1ea3cbbd7bde0a9
LucaLeonx <luca.leonzio@mail.polimi.it>
2024-10-01 17:33:21 +0000

    Added double check translation, doesn't work

--- Challenge_1/question1.cpp
+++ Challenge_1/question1.cpp
@@ -18,18 +18,29 @@ int main(){
     return 1;
     }
 
-    Eigen::MatrixXd originalGrayEinstein(width,height);
-    for(int i=0;i<width;i++){
-        for(int j=0;j<height;j++){
+    Eigen::MatrixXd originalEinstenMat(height,width);
+    for(int i=0;i<height;i++){
+        for(int j=0;j<width;j++){
             int index = (i * width + j) * channels; 
-            originalGrayEinstein(i,j) = static_cast<double>(image_data[index]);
+            originalEinstenMat(i,j) = static_cast<double>(image_data[index]);
         }
     }
 
     //Point 1
     std::cout << "Image loaded: " << width << "x" << height << " with " << channels << " channels." << std::endl;
-    std::cout << "Matrix of the original image: " << originalGrayEinstein << std::endl;
+    //std::cout << "Matrix of the original image: " << originalEinsteinMat << std::endl;
+    std::cout << "Size of the matrix based on the image: " <<originalEinstenMat.size() << std::endl;
     
-    
+    //Check if the first images is translated correctly
+    const std::string output_image_path = "outputImages/testFirstOutput.png";
+    if (stbi_write_png(output_image_path.c_str(), width, height, 1, originalEinstenMat.data(), width) == 0) {
+    std::cerr << "Error: Could not save grayscale image" << std::endl;
+
+    return 1;
+  }
+
+
+    stbi_image_free(image_data);
+
     return 0;
 }
