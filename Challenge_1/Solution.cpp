dd090ecf450b0365a118c13eccb9f3a1f36a9421
LucaLeonx <luca.leonzio@mail.polimi.it>
2024-10-02 19:38:32 +0200

    Point 4 completed, problem of bad_alloc

--- Challenge_1/Solution.cpp
+++ Challenge_1/Solution.cpp
@@ -26,8 +26,51 @@ int printImage(std::string path, int h , int w , Eigen::MatrixXd mat){
   return 0;
 }
 
+//We are assuming a 3x3 filter matrix 
+Eigen::MatrixXd computeConvMatr(int height, int width, MatrixXd filter){
+
+  Eigen::MatrixXd matr = Eigen::MatrixXd::Zero(height*width, height*width);
+  for (int i=0; i < height*width; i++){
+    matr(i,i) = filter(1,1);
+    if(i+4 < height*width){
+      matr(i,i+1) = filter(0,1);
+      matr(i,i+2) = filter(1,2);
+      matr(i,i+3) = filter(1,0);
+      matr(i,i+4) = filter(1,2);
+    }else if(i+3 < height*width){
+      matr(i,i+1) = filter(0,1);
+      matr(i,i+2) = filter(1,2);
+      matr(i,i+3) = filter(1,0);
+    }else if(i+2 < height*width){
+      matr(i,i+1) = filter(0,1);
+      matr(i,i+2) = filter(1,2);
+    }else if(i+1 < height*width){
+      matr(i,i+1) = filter(0,1);
+    }
+
+    if(i-4 >= 0){
+      matr(i,i-4) = filter(0,0);
+      matr(i,i-3) = filter(2,0);
+      matr(i,i-2) = filter(1,0);
+      matr(i,i-1) = filter(2,1);
+    }else if(i-3 >= 0){
+      matr(i,i-3) = filter(1,2);
+      matr(i,i-2) = filter(1,0);
+      matr(i,i-1) = filter(2,1);
+    }else if(i-2 >= 0){
+      matr(i,i-2) = filter(1,0);
+      matr(i,i-1) = filter(1,2);
+    }else if(i-1 >= 0){
+      matr(i,i-1) = filter(1,2);
+    }
+  }
+  return matr;
+}
+
+
 
 int main(){
+  
   const char* input_image_path = "assets/256px-Albert_Einstein_Head.jpg";
 
   int width, height, channels;
@@ -50,7 +93,8 @@ int main(){
   printImage("outputImages/testFirstOutput.png",height,width,originalEinsteinMat);
 
   std::cout << "Image loaded: " << width << "x" << height << " with " << channels << " channels." << std::endl;
-  
+  stbi_image_free(image_data);
+
   //Point2
   Eigen::MatrixXd noise = Eigen::MatrixXd::Random(height,width);
 
@@ -95,10 +139,36 @@ int main(){
     std::cout << "The sizes of the matrix and the vector w DON'T corresponds" << std::endl;
   }
 
+  
+
   std::cout << "The vector v norm is: " << vector_V.norm() <<std::endl;
   
- 
-  stbi_image_free(image_data);
+  //Point4
+  Eigen::MatrixXd smoothedMatrix = Eigen::MatrixXd::Zero(height, width);
+  Eigen::MatrixXd Hav2(3,3);
+  Hav2 << 1/9, 1/9, 1/9,
+          1/9, 1/9, 1/9,
+          1/9, 1/9, 1/9;
+  
+  //calculate convmatrixA1
+  double mTimesn = height*width;
+  Matrix<double, Eigen::Dynamic, Eigen::Dynamic> convMatrixA1(height * width, height * width); //Here is the problem!!!
+  
+  convMatrixA1 = computeConvMatr(height,width,Hav2);
+   
+  smoothedMatrix = convMatrixA1 * vector_V;
+  int numOfNonZeroes = 0;
+  for(int i = 0; i < smoothedMatrix.rows(); i++) {
+      for(int j = 0; j < smoothedMatrix.cols(); j++) {
+          if(smoothedMatrix(i,j) != 0){
+            numOfNonZeroes++;
+          }
+      }
+  }
+  std::cout << "Numer of non-zeroes from the A1*v matrix: " << numOfNonZeroes << " Shoulbe the the order of: " <<height*width <<std::endl;
+  
+  
+  
 
   return 0;
 }
