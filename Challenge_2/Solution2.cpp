e12c58a334a6b955d0f6abd7120bc4573cbab2d0
giolagioia <giovanni.lagioia@mail.polimi.it>
2024-10-16 10:43:08 +0200

    setup Challenge2

---
+++ Challenge_2/Solution2.cpp
@@ -0,0 +1,16 @@
+#include <Eigen/Eigen>
+#include <iostream>
+#include <cstdlib>
+#include <unsupported/Eigen/SparseExtra>
+#include <Eigen/IterativeLinearSolvers>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "../external_libraries/stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "../external_libraries/stb_image_write.h"
+
+using namespace Eigen;
+
+int main(){
+    //Tasks to be completed
+}
